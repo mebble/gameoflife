f7e8a55848e6a2eb059d9b8789077a3665e1055e
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-05-23 19:33:51 +0530

    basic reconcile

--- cypress/integration/player.spec.ts
+++ cypress/integration/player.spec.ts
@@ -1,4 +1,5 @@
-import { neighbourhood } from '../../src/player';
+import { Cell } from '../../src/models';
+import { neighbourhood, reconcile, Position } from '../../src/player';
 
 describe('neighbourhood', () => {
     it('should return neighbours of an x,y pair and itself', () => {
@@ -9,5 +10,35 @@ describe('neighbourhood', () => {
             [[2, 4], [3, 4], [4, 4]],
             [[2, 5], [3, 5], [4, 5]]
         ]);
+    });
+});
+
+describe('reconcile', () => {
+    it('should return all dead cells when no cells are passed in', () => {
+        const positions: Position[] = [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+        ];
+
+        const cells = reconcile(positions, []);
+
+        expect(cells).to.deep.equal([
+            new Cell(0, 0, 'dead'),
+            new Cell(0, 1, 'dead'),
+            new Cell(0, 2, 'dead'),
+        ]);
+    });
+
+    it('should return no cells when no positions are passed in', () => {
+        const cells: Cell[] = [
+            new Cell(0, 0, 'dead'),
+            new Cell(0, 1, 'alive'),
+            new Cell(0, 2, 'dead'),
+        ];
+
+        const newCells = reconcile([], cells);
+
+        expect(newCells).to.deep.equal([]);
     });
 });
--- src/player.ts
+++ src/player.ts
@@ -1,4 +1,6 @@
-type Position = [x: number, y: number];
+import { Cell } from "./models";
+
+export type Position = [x: number, y: number];
 
 export const neighbourhood = (x: number, y: number): Position[][] => {
     return [
@@ -6,4 +8,8 @@ export const neighbourhood = (x: number, y: number): Position[][] => {
         [[x - 1, y    ], [x, y    ], [x + 1, y    ]],
         [[x - 1, y + 1], [x, y + 1], [x + 1, y + 1]]
     ];
+};
+
+export const reconcile = (positions: Position[], cells: Cell[]): Cell[] => {
+    return positions.map(p => new Cell(p[0], p[1], 'dead'));
 };
