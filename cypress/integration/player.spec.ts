1fe2581aba24998d90fefb798b4a2232a6b15bd1
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-05-23 20:33:15 +0530

    return 1d neighbour array

--- cypress/integration/player.spec.ts
+++ cypress/integration/player.spec.ts
@@ -6,9 +6,9 @@ describe('neighbourhood', () => {
         const neighbours = neighbourhood(3, 4);
 
         expect(neighbours).to.deep.equal([
-            [[2, 3], [3, 3], [4, 3]],
-            [[2, 4], [3, 4], [4, 4]],
-            [[2, 5], [3, 5], [4, 5]]
+            [2, 3], [3, 3], [4, 3],
+            [2, 4], [3, 4], [4, 4],
+            [2, 5], [3, 5], [4, 5]
         ]);
     });
 });
