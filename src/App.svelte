97ee757cd768a16ddb2f7187dec36fd6a9e65336
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-05-24 16:27:18 +0530

    restyle

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
     onMount(() => {
         new p5((sketch) => {
             sketch.setup = () => {
-                const canvas = sketch.createCanvas(500, 500);
+                const canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
                 canvas.parent('canvas-container');
             };
 
@@ -40,26 +40,32 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-    <a href="https://www.conwaylife.com/wiki/Conway's_Game_of_Life">Conway's Game of Life Wiki</a><br/>
-    <select name="seed" bind:value={selectedSeed}>
-        {#each seeds as seed}
-            <option value={seed}>{seed.name}</option>
-        {/each}
-    </select><br />
+    <a href="https://www.conwaylife.com/wiki/Conway's_Game_of_Life">Conway's Game of Life Wiki</a>
+    <div id="parameters">
+        <select name="seed" bind:value={selectedSeed}>
+            {#each seeds as seed}
+                <option value={seed}>{seed.name}</option>
+            {/each}
+        </select>
+        <label>Intermediate states: <input type="checkbox" bind:checked={withIntermediates}></label>
+        <label>Strokes: <input type="checkbox" bind:checked={withStrokes}></label>
+    </div>
     {#if selectedSeed}
         <a href={selectedSeed.info}>{selectedSeed.name} Wiki</a>
     {/if}
-    <label>With intermediate states: <input type="checkbox" bind:checked={withIntermediates}></label>
-    <label>With strokes: <input type="checkbox" bind:checked={withStrokes}></label>
     <div id="canvas-container"></div>
 </main>
 
 <style>
+    :global(body) {
+		padding: 0;
+        margin: 0;
+	}
+
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		margin: 0;
+        padding: 0;
 	}
 
 	h1 {
@@ -68,6 +74,24 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+    #parameters {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 1rem .5rem;
+    }
+    #parameters > * {
+        height: max-content;
+        margin: 0;
+    }
+    #parameters > * + * {
+        margin-left: 1rem;
+    }
+
+    #canvas-container {
+        margin-top: 1.5rem;
+    }
 
 	@media (min-width: 640px) {
 		main {
