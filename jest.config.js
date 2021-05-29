330eff11b5beae63196c0bc67365700b7705ac3e
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-05-29 14:19:43 +0530

    use separate tsconfig file for jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json'
+    }
+  }
 };
