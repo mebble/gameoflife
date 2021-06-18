9c3cafa858b5e5195cbeb19d7df702c356a41e75
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-06-18 21:18:24 +0530

    be clever

--- src/player.ts
+++ src/player.ts
@@ -3,11 +3,12 @@ import Cell from './Cell';
 export type Position = [x: number, y: number];
 
 export const neighbourhood = (x: number, y: number): Position[] => {
-    return [
-        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
-        [x - 1, y    ], [x, y    ], [x + 1, y    ],
-        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
+    const steps = [
+        [-1, -1], [0, -1], [1, -1],
+        [-1,  0], [0,  0], [1,  0],
+        [-1,  1], [0,  1], [1,  1],
     ];
+    return steps.map(([ dx, dy ]) => [x + dx, y + dy]);
 };
 
 export const reconcile = (positions: Position[], cells: Cell[]): Cell[] => {
